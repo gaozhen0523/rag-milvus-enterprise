35f08db0c1130368e77df84c95699c994c01c64e
gaozhen0523 <gaozhen0523@gmail.com>
2025-12-07 19:15:02 -0600

    Revert "Day 17： tf format"
    This reverts commit 98e7476320039bb8ae762203011f1ecd15269d17.

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
-  default = [
+  default     = [
     "10.0.1.0/24",
     "10.0.2.0/24"
   ]
@@ -34,7 +34,7 @@ variable "public_subnet_cidrs" {
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default = [
+  default     = [
     "10.0.101.0/24",
     "10.0.102.0/24"
   ]
