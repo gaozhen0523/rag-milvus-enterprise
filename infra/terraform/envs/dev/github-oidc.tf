97fb473975f8e1f73b94e03812e7a13aef8465be
gaozhen0523 <gaozhen0523@gmail.com>
2025-12-09 17:43:51 -0600

    Day 17(1)： add permission

--- infra/terraform/envs/dev/github-oidc.tf
+++ infra/terraform/envs/dev/github-oidc.tf
@@ -65,6 +65,7 @@ data "aws_iam_policy_document" "github_actions_policy" {
     effect = "Allow"
     actions = [
       "ecs:RegisterTaskDefinition",
+      "ecs:DeregisterTaskDefinition"
       "ecs:DescribeTaskDefinition",
       "ecs:DescribeServices",
       "ecs:UpdateService"
